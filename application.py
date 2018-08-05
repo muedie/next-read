88b2ead0d028982af1323f9cdaf71e72e80a0181
muedie <lasafalus@gmail.com>
2018-08-04 19:22:53 -0500

    Initial templates & login logic

--- application.py
+++ application.py
@@ -1,15 +1,20 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+from helpers import login_required
 
 app = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
+
+if not os.getenv("API_KEY"):
+    raise RuntimeError("API_KEY is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -22,6 +27,14 @@ db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
+@login_required
 def index():
 
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """User Log-in Page"""
+
+    return render_template("login.html")
