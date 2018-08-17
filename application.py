f9b2f13084d44282e26f9c0212d5145c2fbbb071
muedie <lasafalus@gmail.com>
2018-08-16 23:21:29 -0500

    Added search logic & book page with simple review functionality

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import login_required
+from helpers import login_required, get_goodreads_reviews
 
 app = Flask(__name__)
 
@@ -27,19 +27,158 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
     """Main page where user can search and click the book for more info"""
 
-    return render_template("index.html")
+    if request.method == "POST":
+        # post request for search
+
+        query = request.form.get("query")
+
+        # ensure there's search text
+        if not query:
+            flash("Empty Search String!")
+            return redirect("/")
+
+        # search for books
+        books = []
+
+        # look by isbn
+        rows = db.execute(
+            "SELECT * FROM books WHERE LOWER(books.isbn) LIKE :query", {"query": '%' + query.lower() + '%'})
+
+        # return out if query matches any isbn
+        if rows.rowcount > 0:
+            books.extend(rows.fetchall())
+            return render_template("index.html", books=books, query=query)
+
+        # look by title
+        rows = db.execute(
+            "SELECT * FROM books WHERE LOWER(books.title) LIKE :query", {"query": '%' + query.lower() + '%'})
+
+        books.extend(rows.fetchall())
+
+        # look by author
+        rows = db.execute(
+            "SELECT * FROM books WHERE LOWER(books.author) LIKE :query", {"query": '%' + query.lower() + '%'})
+
+        books.extend(rows.fetchall())
+
+        if len(books) < 1:
+            flash("No results found!")
+
+        print("QUERY: ", query)
+
+        return render_template("index.html", books=books, query=query)
+
+    else:
+        # get request
+        return render_template("index.html")
 
 
-@app.route("/api/<int:isbn>")
+@app.route("/book/<string:isbn>", methods=["GET", "POST"])
+def book(isbn):
+
+    if request.method == "POST":
+
+        # grab info from form
+        rating = request.form.get("rating")
+        review = request.form.get("review")
+
+        if not rating or int(rating) < 1 or int(rating) > 5:
+            flash("Review Failed, Invalid Rating!")
+        elif not review:
+            flash("Review Failed, Plese enter some text!")
+        else:
+            # we have rating & review
+
+            # check if user already left review
+            rows = db.execute("SELECT * from reviews WHERE book_isbn = :isbn AND user_id = :uid", {
+                              "isbn": isbn, "uid": session["user_id"]})
+
+            if rows.rowcount > 0:
+                flash("Review Failed, Already left a review!")
+            else:
+                # able to post review
+                db.execute('INSERT INTO reviews ("user_id", "review", "rating", "book_isbn") VALUES (:uid, :review, :rating, :isbn)', {
+                    "uid": session["user_id"],
+                    "review": review,
+                    "rating": rating,
+                    "isbn": isbn
+                })
+
+                db.commit()
+
+        # grab book info
+        rows = db.execute(
+            "SELECT * FROM books WHERE books.isbn = :isbn", {"isbn": isbn})
+
+        book = rows.fetchone()
+
+        # get reviews
+        rows = db.execute(
+            "SELECT reviews.*, users.username FROM reviews INNER JOIN users ON reviews.user_id = users.id WHERE book_isbn = :isbn", {"isbn": isbn})
+
+        reviews = rows.fetchall()
+
+        star_string = dict()
+
+        for i in range(len(reviews)):
+
+            rating_string = ''
+
+            for j in range(5):
+                if j < reviews[i][3]:
+                    rating_string += '\u2605 '
+                else:
+                    rating_string += '\u2606 '
+
+            star_string[reviews[i][0]] = rating_string
+
+        return render_template("book.html", book=book, reviews=reviews, star_string=star_string)
+    else:
+
+        # GET request: grab book info
+        rows = db.execute(
+            "SELECT * FROM books WHERE books.isbn = :isbn", {"isbn": isbn})
+
+        book = rows.fetchone()
+
+        # get reviews
+        rows = db.execute(
+            "SELECT reviews.*, users.username FROM reviews INNER JOIN users ON reviews.user_id = users.id WHERE book_isbn = :isbn", {"isbn": isbn})
+
+        reviews = rows.fetchall()
+
+        star_string = dict()
+
+        for i in range(len(reviews)):
+
+            rating_string = ''
+
+            for j in range(5):
+                if j < reviews[i][3]:
+                    rating_string += '\u2605 '
+                else:
+                    rating_string += '\u2606 '
+
+            star_string[reviews[i][0]] = rating_string
+
+        return render_template("book.html", book=book, reviews=reviews, star_string=star_string)
+
+
+@app.route("/api/<string:isbn>")
 def book_info(isbn):
     """ API route for info about book for given ISBN  # """
 
-    return jsonify([isbn])
+    stats = get_goodreads_reviews(isbn)
+
+    return jsonify({
+        "isbn": isbn,
+        **stats
+    })
 
 
 @app.route("/login", methods=["GET", "POST"])
