3a4b1c07c28773c8718060922506d9199cd6cbde
muedie <lasafalus@gmail.com>
2018-08-16 23:20:04 -0500

    goodread api call helper method

--- helpers.py
+++ helpers.py
@@ -1,7 +1,10 @@
 # helper functions for application
 
+import os
 from flask import redirect, session
 from functools import wraps
+import requests
+from xml.etree import ElementTree
 
 
 def login_required(f):
@@ -13,3 +16,19 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+
+def get_goodreads_reviews(isbn):
+    """Makes api call to goodreads API to retrive review stats"""
+
+    res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                       params={"key": os.getenv("API_KEY"), "isbns": isbn})
+
+    if res.status_code == 200:
+        book = res.json()["books"][0]
+        print(book)
+
+    return {
+        "ratings_count": book["work_ratings_count"],
+        "average_rating": book["average_rating"]
+    }
